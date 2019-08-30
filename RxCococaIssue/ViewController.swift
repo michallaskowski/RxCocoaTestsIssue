cc3d8d4744c879244ba55a312d48bc0f24770fb2
Laskowski, Michal <Michal.Laskowski@vimn.com>
2019-08-30 15:38:55 +0200

    Initial Commit

---
+++ RxCococaIssue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxCococaIssue
+//
+//  Created by Laskowski, Michal on 30/08/2019.
+//  Copyright © 2019 Laskowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
